ac6f266931f814942f240b6bef91bd1522ef784f
TechMind <dineshsanthi05@gmail.com>
2018-07-12 22:00:31 -0400

    added support for tomcat server

--- src/main/java/com/repo/depo/RepoDepoApplication.java
+++ src/main/java/com/repo/depo/RepoDepoApplication.java
@@ -1,11 +1,17 @@
 package com.repo.depo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.repo.depo.validator.ApplicationValidator;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class RepoDepoApplication {
 

a7a7c9cae1ffc49f9187cac4c6bf5ca1310c1964
TechMind <dineshsanthi05@gmail.com>
2018-07-12 22:15:22 -0400

    commit

--- src/main/java/com/repo/depo/RepoDepoApplication.java
+++ src/main/java/com/repo/depo/RepoDepoApplication.java
@@ -3,6 +3,7 @@ package com.repo.depo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,7 @@ import com.repo.depo.validator.ApplicationValidator;
 @ComponentScan
 @EnableAutoConfiguration
 @SpringBootApplication
-public class RepoDepoApplication {
+public class RepoDepoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RepoDepoApplication.class, args);
